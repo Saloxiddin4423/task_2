c2e9ef518dff1aeabed2d393f1b0308c04b8eadf
Saloxiddin <97308188+Saloxiddin4423@users.noreply.github.com>
2022-10-28 22:16:51 +0500

    Add files via upload

--- task_2html/conponets/dashboard/js/script.js
+++ task_2html/conponets/dashboard/js/script.js
@@ -12,7 +12,7 @@ class Aside extends HTMLElement {
                 <h3 id="Users" onclick="exchange('Users')"><i class="bi bi-person-circle"></i> Users</h3>
             </div>
             <div class="aside__saidbar-register">
-                <h3  onclick="exchange('Account')"><i   class="bi bi-person-badge-fill"></i> Account</h3>
+                <h3 class="" id="Account" onclick="exchange('Account')"><i   class="bi bi-person-badge-fill"></i> Account</h3>
                 <h3 onclick="exchange('Logout')" ><i class="bi bi-box-arrow-left"></i> Logout</h3>
             </div>
         </div>
@@ -22,7 +22,10 @@ class Aside extends HTMLElement {
 }
 class Card extends HTMLElement {
     connectedCallback() {
-        this.innerHTML = ` <div class="cards">
+
+        this.innerHTML = ` 
+   
+    <div class="cards">
     <div class="card">
         <img src="../../img/card.png" alt="">
         <h3 class="card-title">Headline label </h3>
@@ -32,7 +35,9 @@ class Card extends HTMLElement {
     <div class="card-button">
         <h2><i class="bi bi-three-dots"></i></h2>
     </div>
-</div>`
+</div>
+    </a> 
+   `
     }
 }
 class Navbar extends HTMLElement {
@@ -122,71 +127,251 @@ customElements.define('main-card', Card);
 customElements.define('main-aside', Aside);
 let text = document.getElementById("text")
 let Applicants = document.getElementById("Applicants")
-let section__body = document.getElementById("section__body")
-let section__applicants = document.getElementById("section__applicants")
+let Dashboard = document.getElementById("Dashboard")
 let section = document.getElementById("section")
 let Test = document.getElementById("Test")
-let Dashboard = document.getElementById("Dashboard")
 let Users = document.getElementById("Users")
+let modal = document.getElementById("modal")
+let Account = document.getElementById("Account")
+let containers = document.getElementById("containers")
+let section__body = document.getElementById("section__body")
+let section__applicants = document.getElementById("section__applicants")
 let section__test = document.getElementById("section__test")
 let section__users = document.getElementById("section__users")
-let modal = document.getElementById("modal")
-let containers = document.getElementById("containers")
 let section__test__body__form = document.getElementById("section__test__body__form")
 let section__test__body__form2 = document.getElementById("section__test__body__form2")
+let section__account = document.getElementById("section__account")
+let sectionInput1 = document.getElementById("sectionInput1")
+let inputA = document.getElementById("inputA")
+let inputB = document.getElementById("inputB")
+let inputD = document.getElementById("inputD")
+let turi = document.getElementById("turi")
+let resOnaTili = JSON.parse(localStorage.getItem("OnaTili"))
+let resTarx = JSON.parse(localStorage.getItem("tarx"))
+let resMetematika = JSON.parse(localStorage.getItem("metematika"))
+let a = document.getElementById("a")
+let b = document.getElementById("b")
+let d = document.getElementById("d")
+let OnaTili = []
+let tarx = []
+let metematika = []
+let test = []
+let answers;
 const exchange = (arg) => {
     if (arg == "test") {
         text.style.color = "#4C6FFF"
         Applicants.style.color = "#718096"
-        section__body.style.display = "block"
-        section__applicants.style.display = 'none'
+        section__body.className = "section__body d-block"
+        section__applicants.className = 'd-none'
     } else if (arg == "Applicants") {
         text.style.color = "#718096"
+        section__body.className = "section__body d-none"
         Applicants.style.color = "#4C6FFF"
-        section__body.style.display = "none"
-        section__applicants.style.display = 'block'
+        section__applicants.className = 'd-block section__applicants'
     } else if (arg == "Dashboard") {
-        section.style.display = "block"
-        section__test.style.display = "none"
-        section__users.style.display = "none"
-        Test.className = ""
+        section.className = "d-block section "
+        section__test.className = "d-none"
+        section__users.className = "d-none"
+        section__account.className = "d-none"
         Dashboard.className = "active"
+        Test.className = ""
+        Account.className = ""
         Users.className = ' '
     }
     else if (arg == "Test") {
-        section.style.display = "none"
-        section__test.style.display = "block"
-        section__users.style.display = "none"
+        section__test.className = "d-block section__test"
+        section.className = "d-none"
+        section__account.className = "d-none"
+        section__users.className = "d-none"
         Test.className = "active"
         Dashboard.className = ""
-        Users.className = ' '
-
+        Users.className = ''
+        Account.className = ""
     }
     else if (arg == "Users") {
-        section.style.display = "none"
-        section__test.style.display = "none"
-        section__users.style.display = "block"
+        section__users.className = "d-block"
+        section.className = "d-none"
+        section__test.className = "d-none"
+        section__account.className = "d-none"
+        Users.className = 'active'
         Test.className = ""
         Dashboard.className = ""
-        Users.className = 'active'
+        Account.className = ""
     }
     else if (arg == "openModal") {
-       modal.className = "d-block modal"
-       containers.className = "opasity containers"
+        modal.className = "d-block modal"
+        containers.className = "opasity containers"
 
     }
     else if (arg == "oneOption") {
         modal.className = "d-none"
         containers.className = " containers"
-        section__test__body__form.className="d-block section__test__body__form"
-        section__test__body__form2.className="d-none"
-     }
-     else if (arg == "severalOptions") {
+        section__test__body__form.className = "d-block section__test__body__form"
+        section__test__body__form2.className = "d-none"
+        section__account.className = "d-none"
+    }
+    else if (arg == "severalOptions") {
         modal.className = "d-none"
         containers.className = " containers"
-        section__test__body__form2.className="d-block section__test__body__form2"
-        section__test__body__form.className="d-none"
+        section__test__body__form2.className = "d-block section__test__body__form2"
+        section__test__body__form.className = "d-none"
+        section__account.className = "d-none"
 
-     }
-      
-} 
+    } else if (arg == "Account") {
+        section__account.className = "d-block section__account"
+        section__users.className = "d-none"
+        section__test.className = "d-none"
+        section.className = "d-none"
+        Account.className = "active"
+        Test.className = ""
+        Users.className = ''
+        Dashboard.className = ""
+    }
+}
+
+
+
+const p = () => {
+    if (a.checked) {
+        answers = a.value
+    } else if (b.checked) {
+        answers = b.value
+    }
+    else if (d.checked) {
+        answers = d.value
+    }
+    let person = {
+        name: sectionInput1.value,
+        inputA: inputA.value,
+        inputB: inputB.value,
+        inputD: inputD.value,
+        answer:answers
+
+    }
+    test.push(person)
+    sectionInput1.value = ""
+    inputA.value = ""
+    inputB.value = ""
+    inputD.value = ""
+}
+const addTest = () => {
+    if (turi.value == "OnaTili") {
+        if (localStorage.getItem("OnaTili")) {
+            OnaTili = []
+            JSON.parse(localStorage.getItem("OnaTili")).forEach(elem => {
+                OnaTili.push(elem)
+            });
+            OnaTili.push(test)
+            localStorage.setItem("OnaTili", JSON.stringify(OnaTili))
+        } else {
+            OnaTili.push(test)
+            localStorage.setItem("OnaTili", JSON.stringify(OnaTili))
+        }
+
+        location.reload()
+    }
+    else if (turi.value == "tarx") {
+        if (localStorage.getItem("tarx")) {
+            tarx = []
+            JSON.parse(localStorage.getItem("tarx")).forEach(elem => {
+                tarx.push(elem)
+            });
+            tarx.push(test)
+            localStorage.setItem("tarx", JSON.stringify(tarx))
+        } else {
+            tarx.push(test)
+            localStorage.setItem("tarx", JSON.stringify(tarx))
+        }
+
+        location.reload()
+    }
+    else if (turi.value == "metematika") {
+        if (localStorage.getItem("metematika")) {
+            metematika = []
+            JSON.parse(localStorage.getItem("metematika")).forEach(elem => {
+                metematika.push(elem)
+            });
+            metematika.push(test)
+            localStorage.setItem("metematika", JSON.stringify(metematika))
+        } else {
+            metematika.push(test)
+            localStorage.setItem("metematika", JSON.stringify(metematika))
+        }
+
+        location.reload()
+    }
+}
+if (localStorage.getItem("OnaTili")) {
+    resOnaTili.forEach((elem, index) => {
+        section__body.innerHTML += `
+        <a href="../../index.html"  onclick="access('${index}','onatili')">
+        <div class="cards">
+          <div class="card">
+             <img src="../../img/onatili.png" alt="">
+             <h3 class="card-title">Ona-tili</h3>
+             <p class="card-label">Label, Label</p>
+             <button class="card-btn">Badge</button>
+          </div>
+          <div class="card-button">
+              <h2><i class="bi bi-three-dots"></i></h2>
+          </div>
+        </div></a>
+    `
+    });
+}
+if (localStorage.getItem("tarx")) {
+    resTarx.forEach((elem, index) => {
+        section__body.innerHTML += `
+            <a href="../../index.html" onclick="access(${index},'tarx')">
+            <div class="cards" >
+              <div class="card">
+                 <img src="../../img/unnamed.webp" alt="">
+                 <h3 class="card-title">Tarix</h3>
+                 <p class="card-label">Label, Label</p>
+                 <button class="card-btn">Badge</button>
+              </div>
+              <div class="card-button">
+                  <h2><i class="bi bi-three-dots"></i></h2>
+              </div>
+            </div></a>
+        `
+    });
+}
+if (localStorage.getItem("metematika")) {
+    resMetematika.forEach((elem, index) => {
+        section__body.innerHTML += `
+            <a href="../../index.html" onclick="access(${index},'matem')">
+            <div class="cards"  >
+              <div class="card">
+                 <img src="../../img/matem.png" alt="">
+                 <h3 class="card-title">Matematika </h3>
+                 <p class="card-label">Label, Label</p>
+                 <button class="card-btn">Badge</button>
+              </div>
+              <div class="card-button">
+                  <h2><i class="bi bi-three-dots"></i></h2>
+              </div>
+            </div></a>
+        `
+    });
+}
+
+
+
+
+
+const access = (id, science) => {
+    if (science == 'onatili') {
+        localStorage.setItem("id", id)
+        localStorage.setItem("science", 'OnaTili')
+    } else if (science == "tarx") {
+        localStorage.setItem("id", id)
+        localStorage.setItem("science", 'tarx')
+    }
+    else if (science == "matem") {
+        localStorage.setItem("id", id)
+        localStorage.setItem("science", "metematika")
+    }
+
+}
+
